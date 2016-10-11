d9c52568bc85e7457dba7d22f91b2d13d094af5a
1-718-8 <1-718-8@02-717-8-PC>
2016-10-11 13:55:37 +0900

    멤버 에디터 업데이트 모델 추가

--- src/main/java/com/whattodo/repo/MemberRepo.java
+++ src/main/java/com/whattodo/repo/MemberRepo.java
@@ -5,6 +5,7 @@ import com.whattodo.dto.Member;
 public interface MemberRepo {
 	public int insertMember(Member member);
 	public int updateMember(Member member);
+	public int updateMemberEditor(String id);
 	public int deleteMember(String id);
 	public Member getMemberById(String id);
 	public Member getMemberByEmail(String email);
